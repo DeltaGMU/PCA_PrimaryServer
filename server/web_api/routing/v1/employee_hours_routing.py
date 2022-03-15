8a90f3c3bd9f3a1d6f2b698e95590680af008970
Jason Jerome <jasonjerome16@gmail.com>
2022-03-15 15:27:17 -0400

    Added docs for employee time sheets

--- server/web_api/routing/v1/employee_hours_routing.py
+++ server/web_api/routing/v1/employee_hours_routing.py
@@ -58,7 +58,7 @@ class EmployeeHoursRouter:
             Front-end interaction can send requests to this endpoint with any valid date range, which would
             be useful for presenting total work hours over the course of a week, 2 weeks, a month, or a year.
 
-            :param employee_id: The ID of the employee
+            :param employee_id: The ID of the employee.
             :type employee_id: str, required
             :param employee_hours: The starting date and the ending date of the total hours to be calculated.
             :type employee_hours: PydanticReadEmployeeTimesheet, required
@@ -80,13 +80,28 @@ class EmployeeHoursRouter:
             if employee_hours.date_end is None:
                 employee_hours.date_end = employee_hours.date_start
             total_hours_and_list = await get_employee_hours_total(employee_id.strip(), employee_hours.date_start, employee_hours.date_end, session)
-
             return ResponseModel(status.HTTP_200_OK, "success", total_hours_and_list)
 
     class Update:
         @staticmethod
         @router.put(ENV_SETTINGS.API_ROUTES.Timesheet.one_timesheet, status_code=status.HTTP_200_OK)
         async def update_employee_time_sheet(employee_id: str, updated_employee_hours: PydanticEmployeeTimesheetUpdate, token: str = Depends(oauth_scheme), session=Depends(get_db_session)):
+            """
+            An endpoint to update the total work hours, pto hours, overtime/extra hours for an employee on a provided date.
+            Front-end interaction can send requests to this endpoint with any valid date in YYYY-MM-DD format.
+
+            :param employee_id: The ID of the employee.
+            :type employee_id: str, required
+            :param updated_employee_hours: The date and the updated work/pto/extra hours.
+            :type updated_employee_hours: PydanticEmployeeTimesheetUpdate, required
+            :param token: The JSON Web Token responsible for authenticating the user to this endpoint.
+            :type token: str, required
+            :param session: The database session to use to update the timesheet data.
+            :type session: sqlalchemy.orm.session, optional
+            :return: A response model containing the updated total number of work hours, PTO hours, and extra/overtime hours for the employee on the given date.
+            :rtype: server.web_api.models.ResponseModel
+            :raises HTTPException: If the provided employee has no hours logged into the system, or the employee does not exist in the database.
+            """
             if not await token_is_valid(token, ["teacher"]):
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired or is invalid!")
             if employee_id is None or not isinstance(employee_id, str):
@@ -102,8 +117,24 @@ class EmployeeHoursRouter:
     class Delete:
         @staticmethod
         @router.delete(ENV_SETTINGS.API_ROUTES.Timesheet.one_timesheet, status_code=status.HTTP_200_OK)
-        async def delete_employee_time_sheet(employee_id: str, delete_employee_hours: PydanticEmployeeTimesheetRemoval,
-                                             token: str = Depends(oauth_scheme), session=Depends(get_db_session)):
+        async def delete_employee_time_sheets(employee_id: str, delete_employee_hours: PydanticEmployeeTimesheetRemoval,
+                                              token: str = Depends(oauth_scheme), session=Depends(get_db_session)):
+            """
+            An endpoint to update the total work hours, pto hours, overtime/extra hours for an employee on a provided date.
+            Front-end interaction can send requests to this endpoint with any valid date in YYYY-MM-DD format.
+
+            :param employee_id: The ID of the employee.
+            :type employee_id: str, required
+            :param delete_employee_hours: The time sheets to delete on the provided date(s).
+            :type delete_employee_hours: PydanticEmployeeTimesheetUpdate, required
+            :param token: The JSON Web Token responsible for authenticating the user to this endpoint.
+            :type token: str, required
+            :param session: The database session to use to update the timesheet data.
+            :type session: sqlalchemy.orm.session, optional
+            :return: A response model containing the deleted time sheets from the provided date(s).
+            :rtype: server.web_api.models.ResponseModel
+            :raises HTTPException: If the provided employee has no hours logged into the system, or the employee does not exist in the database.
+            """
             if not await token_is_valid(token, ["teacher"]):
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired or is invalid!")
             if employee_id is None or not isinstance(employee_id, str):
@@ -113,41 +144,3 @@ class EmployeeHoursRouter:
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="The user does not have sufficient permissions.")
             deleted_time_sheets = await delete_employee_time_sheets(employee_id, delete_employee_hours, session)
             return ResponseModel(status.HTTP_200_OK, "success", {"time_sheet": deleted_time_sheets})
-
-
-'''
-@router.post("/api/v1/employees/hours/add", status_code=status.HTTP_201_CREATED)
-def add_employee_hours(pyd_employee_hours: PydanticEmployeeHours, session=Depends(get_db_session)):
-    """
-    An endpoint to add work hours for an employee on a specified date. Work hours cannot be added multiple times for the same day.
-
-    :param pyd_employee_hours: The Pydantic EmployeeHours reference. This means that HTTP requests to this endpoint must include the required fields in the Pydantic EmployeeHours class.
-    :type pyd_employee_hours: PydanticEmployeeHours
-    :return: A response model containing the employee and employee hours data that has been added to the database.
-    :rtype: server.web_api.models.ResponseModel
-    :raises HTTPException: If the hours being added on the provided date is a duplicate entry, or the provided data is invalid.
-    """
-
-    try:
-        work_hours_exists = session.query(EmployeeHours).filter(
-            EmployeeHours.EmployeeID == pyd_employee_hours.employee_id,
-            EmployeeHours.DateWorked == pyd_employee_hours.date_worked
-        ).all()
-        if len(work_hours_exists) == 0:
-            total_employee_hours = EmployeeHours(
-                pyd_employee_hours.employee_id,
-                pyd_employee_hours.hours_worked,
-                pyd_employee_hours.date_worked
-            )
-            session.add(total_employee_hours)
-            session.commit()
-        else:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Duplicate date entry! The employee already has hours entered for this day!")
-    except IntegrityError as err:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err)) from err
-    created_employee_hours = session.query(EmployeeHours).filter(
-        EmployeeHours.EmployeeID == pyd_employee_hours.employee_id,
-        EmployeeHours.DateWorked == pyd_employee_hours.date_worked
-    ).one()
-    return ResponseModel(status.HTTP_201_CREATED, "success", {"employee_hours": created_employee_hours.as_detail_dict()})
-'''
