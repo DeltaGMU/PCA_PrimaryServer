efedb4ad492caecfe75388265566f0f6edb8fa82
Jason Jerome <jasonjerome16@gmail.com>
2022-03-19 11:37:31 -0400

    Updated reset token field name

--- server/lib/data_classes/reset_token.py
+++ server/lib/data_classes/reset_token.py
@@ -13,13 +13,13 @@ class PydanticResetToken(BaseModel):
 class ResetToken(Base):
     __tablename__ = 'reset_tokens'
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
-    Token = Column(VARCHAR(length=256), unique=True, nullable=False)
+    ResetToken = Column(VARCHAR(length=256), unique=True, nullable=False)
     EmployeeID = Column(VARCHAR(length=50), ForeignKey('employee.EmployeeID'), unique=True, nullable=False)
     Iss = Column(Integer, nullable=False)
     Exp = Column(Integer, nullable=False)
 
     def __init__(self, token: str, employee_id: str, iss: int, exp: int):
-        self.Token = token
+        self.ResetToken = token
         self.EmployeeID = employee_id
         self.Iss = iss
         self.Exp = exp
@@ -27,7 +27,7 @@ class ResetToken(Base):
     def as_dict(self):
         return {
             "id": self.id,
-            "token": self.Token,
+            "token": self.ResetToken,
             "employee_id": self.EmployeeID,
             "iss": self.Iss,
             "exp": self.Exp
