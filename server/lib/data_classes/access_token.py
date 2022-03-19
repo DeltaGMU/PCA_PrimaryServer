4ef75acfe424fb13d0a5b274688d178bd04d855e
Jason Jerome <jasonjerome16@gmail.com>
2022-03-19 11:37:23 -0400

    Updated access token field name

--- server/lib/data_classes/access_token.py
+++ server/lib/data_classes/access_token.py
@@ -5,19 +5,19 @@ from server.lib.database_access.sqlalchemy_base_interface import MainEngineBase 
 class TokenBlacklist(Base):
     __tablename__ = 'token_blacklist'
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
-    Token = Column(VARCHAR(length=256), unique=True, nullable=False)
+    AccessToken = Column(VARCHAR(length=256), unique=True, nullable=False)
     Iss = Column(Integer, nullable=False)
     Exp = Column(Integer, nullable=False)
 
     def __init__(self, token: str, iss: int, exp: int):
-        self.Token = token
+        self.AccessToken = token
         self.Iss = iss
         self.Exp = exp
 
     def as_dict(self):
         return {
             "id": self.id,
-            "token": self.Token,
+            "token": self.AccessToken,
             "iss": self.Iss,
             "exp": self.Exp
         }
