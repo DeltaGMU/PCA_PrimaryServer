fc2b94df86b59471aceca06b03e77c95bdaa37d2
Jason Jerome <jasonjerome16@gmail.com>
2022-03-21 05:09:49 -0400

    The database is created along with all tables if missing.

--- server/lib/database_access/sqlalchemy_base_interface.py
+++ server/lib/database_access/sqlalchemy_base_interface.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.scoping import scoped_session
 from config import ENV_SETTINGS
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import create_database, database_exists
 
 
 main_engine = create_engine(
@@ -10,5 +11,10 @@ main_engine = create_engine(
     echo=ENV_SETTINGS.db_debug_mode,
     pool_recycle=3600
 )
+if not database_exists(main_engine.url):
+    create_database(main_engine.url)
+else:
+    main_engine.connect()
+
 MainEngineBase = declarative_base(bind=main_engine)
 main_db_session = scoped_session(sessionmaker(bind=main_engine, autoflush=False, autocommit=False))
