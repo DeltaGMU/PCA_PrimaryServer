5cc7e03fd06dc95f222890bb0f621d69034bbcdb
Jason Jerome <jasonjerome16@gmail.com>
2022-03-21 05:10:32 -0400

    Added entry_created fields to access/reset tokens, etc.
    - Also added detail_dict utility methods to access/reset tokens.

--- server/lib/data_classes/access_token.py
+++ server/lib/data_classes/access_token.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy import Column, Integer, VARCHAR, DateTime, sql
 from server.lib.database_access.sqlalchemy_base_interface import MainEngineBase as Base
 
 
@@ -8,6 +8,7 @@ class TokenBlacklist(Base):
     AccessToken = Column(VARCHAR(length=256), unique=True, nullable=False)
     Iss = Column(Integer, nullable=False)
     Exp = Column(Integer, nullable=False)
+    EntryCreated = Column(DateTime, nullable=False, default=sql.func.now())
 
     def __init__(self, token: str, iss: int, exp: int):
         self.AccessToken = token
@@ -20,4 +21,13 @@ class TokenBlacklist(Base):
             "token": self.AccessToken,
             "iss": self.Iss,
             "exp": self.Exp
+        }
+
+    def as_detail_dict(self):
+        return {
+            "id": self.id,
+            "token": self.AccessToken,
+            "iss": self.Iss,
+            "exp": self.Exp,
+            "entry_created": self.EntryCreated
         }
--- server/lib/data_classes/reset_token.py
+++ server/lib/data_classes/reset_token.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy import Column, Integer, VARCHAR, DateTime, sql
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.orm import relationship
 from pydantic import BaseModel
@@ -13,16 +13,27 @@ class PydanticResetToken(BaseModel):
 class ResetToken(Base):
     __tablename__ = 'reset_tokens'
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
+    EmployeeID = Column(VARCHAR(length=50), ForeignKey('employee.EmployeeID'), nullable=False)
     ResetToken = Column(VARCHAR(length=256), unique=True, nullable=False)
-    EmployeeID = Column(VARCHAR(length=50), ForeignKey('employee.EmployeeID'), unique=True, nullable=False)
     Iss = Column(Integer, nullable=False)
     Exp = Column(Integer, nullable=False)
+    EntryCreated = Column(DateTime, nullable=False, default=sql.func.now())
 
     def __init__(self, token: str, employee_id: str, iss: int, exp: int):
         self.ResetToken = token
         self.EmployeeID = employee_id
         self.Iss = iss
         self.Exp = exp
+
+    def as_detail_dict(self):
+        return {
+            "id": self.id,
+            "token": self.ResetToken,
+            "employee_id": self.EmployeeID,
+            "iss": self.Iss,
+            "exp": self.Exp,
+            "entry_created": self.EntryCreated
+        }
 
     def as_dict(self):
         return {
