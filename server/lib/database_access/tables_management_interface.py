fa92ca2c46ee35fbbe7550b3a65abde0a1b51a7b
Jason Jerome <jasonjerome16@gmail.com>
2022-03-21 05:09:36 -0400

    Updated table creation

--- server/lib/database_access/tables_management_interface.py
+++ server/lib/database_access/tables_management_interface.py
@@ -16,14 +16,17 @@ from server.lib.database_manager import get_db_session, MainEngineBase, main_eng
 def initialize_tables():
     if MainEngineBase is None:
         return
-    ResetToken.__table__.create(bind=main_engine, checkfirst=True)
-    TokenBlacklist.__table__.create(bind=main_engine, checkfirst=True)
+    '''
     ContactInfo.__table__.create(bind=main_engine, checkfirst=True)
     Employee.__table__.create(bind=main_engine, checkfirst=True)
     EmployeeHours.__table__.create(bind=main_engine, checkfirst=True)
     EmployeeRole.__table__.create(bind=main_engine, checkfirst=True)
     Student.__table__.create(bind=main_engine, checkfirst=True)
     StudentCareHours.__table__.create(bind=main_engine, checkfirst=True)
+    ResetToken.__table__.create(bind=main_engine, checkfirst=True)
+    TokenBlacklist.__table__.create(bind=main_engine, checkfirst=True)
+    '''
+    MainEngineBase.metadata.create_all()
     LoggingManager().log(LoggingManager.LogLevel.LOG_INFO, 'Initialized database tables.', origin=LOG_ORIGIN_DATABASE, no_print=False)
 
 
