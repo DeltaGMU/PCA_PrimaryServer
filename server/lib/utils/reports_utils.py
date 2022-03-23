4a11a6dc5f6bfde5eb871ceffddf185a535d0393
Jason Jerome <jasonjerome16@gmail.com>
2022-03-23 01:26:50 -0400

    Added employees/students directories for report storage

--- server/lib/utils/reports_utils.py
+++ server/lib/utils/reports_utils.py
@@ -5,5 +5,8 @@ from os import path, makedirs
 def create_reports_directory():
     if not path.exists(f"{ROOT_DIR}/reports"):
         makedirs(f"{ROOT_DIR}/reports")
-
+    if not path.exists(f"{ROOT_DIR}/reports/employees"):
+        makedirs(f"{ROOT_DIR}/reports/employees")
+    if not path.exists(f"{ROOT_DIR}/reports/students"):
+        makedirs(f"{ROOT_DIR}/reports/students")
 
