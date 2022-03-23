d2b66dafe61d887e8574100ebfdb6860676c2cee
Jason Jerome <jasonjerome16@gmail.com>
2022-03-23 05:01:54 -0400

    Updated report generation system
    - Changed weasyprint dependency to pdfkit for better compatibility.
    - Added comments data from timesheet submissions, although they are not currently displayed in the reports.
    - Optimized queries to retrieve timesheet information.
    - Administrators no longer show up in the timesheet reports since they don't log hours.
    - Updated HTML/CSS

--- server/lib/database_access/analytics_interface.py
+++ server/lib/database_access/analytics_interface.py
@@ -1,59 +1,58 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
+
+from server.lib.data_classes.employee_role import EmployeeRole
 from server.lib.data_classes.employee import Employee
 from server.lib.data_classes.employee_hours import EmployeeHours
 from server.lib.database_manager import get_db_session
-import calendar
-import datetime
+from datetime import datetime
 
 
 async def get_all_time_sheets_for_report(start_date: str, end_date: str, session: Session = None):
     if session is None:
         session = next(get_db_session())
     try:
+
+        employee_time_sheet_records = session.query(Employee, EmployeeHours, EmployeeRole).filter(
+            Employee.EmployeeEnabled == 1,
+            EmployeeRole.id == Employee.EmployeeRoleID,
+            EmployeeRole.Name != 'administrator',
+            EmployeeHours.EmployeeID == Employee.EmployeeID,
+            EmployeeHours.DateWorked.between(start_date, end_date)
+        ).order_by(Employee.FirstName).all()
+        if employee_time_sheet_records is None:
+            raise RuntimeError("Encountered an error retrieving employee time sheets!")
+
         all_employees_hours = {}
-        all_employees = session.query(Employee).filter(Employee.EmployeeEnabled == 1).all()
-        for employee in all_employees:
-            time_sheets = session.query(EmployeeHours).filter(
-                EmployeeHours.EmployeeID == employee.EmployeeID,
-                EmployeeHours.DateWorked.between(start_date, end_date),
-            ).all()
-            total_hours = {
+        for record in employee_time_sheet_records:
+            all_employees_hours[record[0].EmployeeID] = {
+                "full_name": f"{record[0].FirstName.capitalize()} {record[0].LastName.capitalize()}",
                 "work_hours": 0,
                 "pto_hours": 0,
                 "extra_hours": 0,
+                "comments": []
             }
-            for record in time_sheets:
-                total_hours['work_hours'] += record.WorkHours
-                total_hours['pto_hours'] += record.PTOHours
-                total_hours['extra_hours'] += record.ExtraHours
-            all_employees_hours[employee.EmployeeID] = total_hours
+
+        for record in employee_time_sheet_records:
+            all_employees_hours[record[0].EmployeeID]["work_hours"] += record[1].WorkHours
+            all_employees_hours[record[0].EmployeeID]["pto_hours"] += record[1].PTOHours
+            all_employees_hours[record[0].EmployeeID]["extra_hours"] += record[1].ExtraHours
+            if record[1].Comment and len(record[1].Comment) > 0:
+                all_employees_hours[record[0].EmployeeID]["comments"].append({"date": datetime.strftime(record[1].DateWorked, '%Y-%m-%d'), "comment": record[1].Comment})
+
         session.commit()
     except IntegrityError as err:
         raise RuntimeError from err
     return all_employees_hours
 
 
-async def check_time_sheets_submitted(session: Session = None):
+async def get_employees_within_reporting_period(start_date: str, end_date: str, session: Session = None):
     if session is None:
         session = next(get_db_session())
-
-    fully_submitted = []
-    not_submitted = []
-
-    now = datetime.datetime.now()
-    days_in_cur_month = calendar.monthrange(now.year, now.month)[1]
-
-    all_employees = session.query(Employee).filter(Employee.EmployeeEnabled == 1).all()
-
-    for employee in all_employees:
-        matching_records = session.query(EmployeeHours).filter(
-            EmployeeHours.EmployeeID == employee.EmployeeID,
-            EmployeeHours.DateWorked.between(f"{now.year}-{now.month}-1", f"{now.year}-{now.month}-{days_in_cur_month}")
-        ).count()
-        if matching_records == days_in_cur_month:
-            fully_submitted.append(employee)
-        else:
-            not_submitted.append(employee)
+    all_employees = session.query(Employee, EmployeeHours).filter(
+        Employee.EmployeeEnabled == 1,
+        EmployeeHours.EmployeeID == Employee.EmployeeID,
+        EmployeeHours.DateWorked.between(start_date, end_date)
+    ).all()
     session.commit()
-    return all_employees, len(fully_submitted), len(not_submitted)
+    return all_employees[0]
