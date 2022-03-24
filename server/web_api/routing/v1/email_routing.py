1d94f6439df81eb49e9490e94641f3c34aa0d559
Jason Jerome <jasonjerome16@gmail.com>
2022-03-24 02:54:46 -0400

    Removed unused imports in email router

--- server/web_api/routing/v1/email_routing.py
+++ server/web_api/routing/v1/email_routing.py
@@ -4,12 +4,7 @@ from fastapi_utils.inferring_router import InferringRouter
 from config import ENV_SETTINGS
 from server.lib.utils.email_utils import send_test_email
 from server.web_api.models import ResponseModel
-from server.lib.data_classes.employee import Employee, PydanticEmployeeRegistration, PydanticEmployeesRemoval, PydanticEmployeeUpdate, \
-    PydanticRetrieveMultipleEmployees, PydanticMultipleEmployeesUpdate
-from server.lib.database_manager import get_db_session
-from server.lib.database_access.employee_interface import get_employee_role, get_employee_contact_info, get_all_employees, get_employee, \
-    create_employee, remove_employees, update_employee, get_multiple_employees, update_employees, is_admin
-from server.web_api.web_security import token_is_valid, oauth_scheme, get_user_from_token
+from server.web_api.web_security import token_is_valid, oauth_scheme
 
 router = InferringRouter()
 
