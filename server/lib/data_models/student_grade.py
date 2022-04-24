41f9a2411b36eb616a8b27437cad785ccdcbd2ee
Jason Jerome <jasonjerome16@gmail.com>
2022-04-23 21:57:58 -0400

    Updated documentation for student grade model

--- server/lib/data_models/student_grade.py
+++ server/lib/data_models/student_grade.py
@@ -1,3 +1,12 @@
+"""
+This module contains the MariaDB data classes and Pydantic data classes for the student grade entity.
+The MariaDB data classes are used in database queries and transactions.
+The Pydantic data classes are used in requests to the API for ensuring that data received and sent through requests are valid.
+For example, creating a new student grade level record through a request to the API will
+require a Pydantic student grade data class to define the attributes needed
+to create the student grade level entity in the database and validate the data that is sent in the request.
+"""
+
 from __future__ import annotations
 from pydantic import BaseModel
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Date, LargeBinary, VARCHAR, Boolean, Time, sql
@@ -6,21 +15,23 @@ from server.lib.database_controllers.sqlalchemy_base_interface import MainEngine
 
 
 class PydanticStudentGrade(BaseModel):
+    """
+    A Pydantic class used to represent a student grade level when creating a new student grade record from an HTTP request to the API.
+    Do not try to initialize this class as an independent entity or extend it into a subclass.
+    """
     student_grade: str
 
 
 class StudentGrade(Base):
     """
     A MariaDB data class that represents the table structure of the student grade table in the database server.
-    This is replicated in the server code to ensure that the data being sent to and received from the database are valid.
-    Do not attempt to manually modify this class or extend it into a subclass.
+    This model is used to generate the student_grade table in the MariaDB database server.
     """
     __tablename__ = 'student_grade'
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
     Name = Column(VARCHAR(length=100), unique=True, nullable=False)
     StudentRelationship = relationship('Student')
 
-    # Do not initialize this except for creating blank student grade templates!
     def __init__(self, grade_name: str):
         """
         The constructor for the ``Grade`` data class that is utilized internally by the SQLAlchemy library.
