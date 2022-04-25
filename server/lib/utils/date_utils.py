8033728d780214948c0c84c655c48187f76966a8
Jason Jerome <jasonjerome16@gmail.com>
2022-04-24 21:38:44 -0400

    Added documentation for date utilities

--- server/lib/utils/date_utils.py
+++ server/lib/utils/date_utils.py
@@ -1,8 +1,22 @@
+"""
+This module contains utility functions related to the validation
+and formatting of date strings to ensure that a date is compatible with server processes.
+"""
+
 from datetime import datetime
 from typing import List, Union
 
 
 def check_date_formats(dates: Union[List[str], str]) -> bool:
+    """
+    This utility method is used to verify that one or more provided date strings
+    are in the YYYY-MM-DD format that the server uses for all date-related functionality.
+
+    :param dates: A list of date strings, or a single date string to be validated.
+    :type dates: List[str] | str
+    :return: True if all the provided dates are in the YYYY-MM-DD format.
+    :rtype: bool
+    """
     if dates is None:
         return False
     if isinstance(dates, str):
