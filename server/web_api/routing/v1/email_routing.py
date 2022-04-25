be9708a040cfb48be2d660ddf6f006e4d54d3534
Jason Jerome <jasonjerome16@gmail.com>
2022-04-24 22:08:05 -0400

    Added documentation to email routing

--- server/web_api/routing/v1/email_routing.py
+++ server/web_api/routing/v1/email_routing.py
@@ -16,6 +16,15 @@ class EmailRouter:
         @staticmethod
         @router.post(API_ROUTES.Email.send_test_email, status_code=status.HTTP_200_OK)
         async def test_email(token: str = Depends(oauth_scheme)):
+            """
+            An endpoint that sends a test email from the server's SmarterMail email account to itself.
+            This endpoint is useful to verify that email communications are fully functional.
+
+            :param token: The JSON Web Token responsible for authenticating the user to this endpoint.
+            :type token: str, required
+            :return: A response model containing the JSON response of the test email that was sent.
+            :rtype: server.web_api.models.ResponseModel
+            """
             if not await token_is_valid(token, ["administrator"]):
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired or is invalid!")
             json_resp = send_test_email()
