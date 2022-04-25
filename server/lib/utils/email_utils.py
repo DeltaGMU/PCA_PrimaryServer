dc2d1416dd4a006d310c0dafb08df0c90d87bb28
Jason Jerome <jasonjerome16@gmail.com>
2022-04-24 21:47:57 -0400

    Added documentation for email utilities

--- server/lib/utils/email_utils.py
+++ server/lib/utils/email_utils.py
@@ -1,3 +1,8 @@
+"""
+This module contains utility functions related to the creation and sending
+of emails using the SmarterMail API.
+"""
+
 import re
 from typing import List
 import requests
@@ -6,6 +11,7 @@ from server.lib.config_manager import ConfigManager
 from server.lib.strings import ROOT_DIR
 from server.web_api.api_routes import THIRD_PARTY_ROUTES
 
+# Email validation regex
 email_validator_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 env = Environment(loader=FileSystemLoader(
     [
@@ -15,6 +21,13 @@ env = Environment(loader=FileSystemLoader(
 
 
 def send_test_email():
+    """
+    This is a test method used to verify that communication between the python server and the SmarterMail API and mail server
+    is working and fully functional by sending a test email from the server's SmarterMail account to itself.
+
+    :return: A JSON-Compatible dictionary of the SmarterMail API response to sending a test email.
+    :rtype: Dict[str, any]
+    """
     email_api = f"{'https://' if ConfigManager().config().getboolean('Email Settings', 'pca_email_use_https') else 'http://'}{ConfigManager().config()['Email Settings']['pca_email_api']}"
     # Authenticate self first...
     auth_request = requests.post(f"{email_api}{THIRD_PARTY_ROUTES.Email.login}", data={
@@ -36,7 +49,28 @@ def send_test_email():
     return email_request.json()
 
 
-def send_email(to_user: str, to_email: List[str], subj: str, messages: List[str], template: str = None, to_cc: str = None):
+def send_email(to_user: str, to_email: List[str], subj: str, messages: List[str], template: str = None, to_cc: str = None) -> bool:
+    """
+    This utility method serves as an abstraction to the SmarterMails message-put endpoint to provide
+    an integration with the python server to allow emails to be sent through SmarterEmail email servers easily.
+    All email communications in this python server must utilize this utility method to send emails.
+
+    :param to_user: The receiving user's name.
+    :type to_user: str, required
+    :param to_email: A list of all receiving user's email addresses.
+    :type to_email: List[str], required
+    :param subj: The subject text of the email.
+    :type subj: str, required
+    :param messages: A list of all the individual messages that should be formatted and rendered in the sent email.
+    :type messages: List[str], required
+    :param template: Optionally the email template can be specified by the file name. For example, leave requests use a different email template than generic emails.
+    :type template: str, optional
+    :param to_cc: Optionally a CC email address can be provided.
+    :type to_cc: str, optional
+    :return: True if the email was successfully formatted, rendered, and sent to the appropriate user(s).
+    :rtype: bool
+    :raises RuntimeError: If any of the provided parameters are invalid, or the email configuration settings in the server configuration file is invalid.
+    """
     email_api = f"{'https://' if ConfigManager().config().getboolean('Email Settings', 'pca_email_use_https') else 'http://'}{ConfigManager().config()['Email Settings']['pca_email_api']}"
     # Validate provided information and email address.
     if None in (to_user, to_email, subj, messages):
