6acd3383e52c3ddada4eef12c365a876a764da7e
Jason Jerome <jasonjerome16@gmail.com>
2022-04-24 21:33:46 -0400

    Added documentation for student grades interface

--- server/lib/database_controllers/student_grades_interface.py
+++ server/lib/database_controllers/student_grades_interface.py
@@ -1,3 +1,10 @@
+"""
+This module contains the functions that interface with the database server
+to handle the processing of student grade levels. Any code related to the handling of
+student grade level records that require creating, reading, updating, or deleting data from the database server
+must use this interface module.
+"""
+from typing import List
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
@@ -7,14 +14,33 @@ from server.lib.data_models.student_grade import StudentGrade, PydanticStudentGr
 from server.lib.database_manager import get_db_session
 
 
-async def retrieve_all_grades(session: Session = None):
+async def retrieve_all_grades(session: Session = None) -> List[StudentGrade]:
+    """
+    This method is used to retrieve all the student grade levels stored in the database.
+
+    :param session: The database session used to retrieve all the student grade levels.
+    :type session: Session, optional
+    :return: A list of all the student grade levels.
+    :rtype: List[StudentGrade]
+    """
     if session is None:
         session = next(get_db_session())
     all_grades = session.query(StudentGrade).all()
     return all_grades
 
 
-async def retrieve_one_grade(grade_name: str, session: Session = None):
+async def retrieve_one_grade(grade_name: str, session: Session = None) -> StudentGrade:
+    """
+    This method is used to retrieve a single grade level record from the database provided the name of the grade level.
+
+    :param grade_name: The name of the student grade level to retrieve from the database.
+    :type grade_name: str, required
+    :param session: The database session used to retrieve a student grade level record from the database.
+    :type session: Session, optional
+    :return: A student grade level record
+    :rtype: StudentGrade
+    :raises HTTPException: If any of the provided parameters is invalid, or the student grade level does not exist.
+    """
     if grade_name is None or len(grade_name) == 0:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The provided student grade is invalid or empty!")
     if session is None:
@@ -24,11 +50,22 @@ async def retrieve_one_grade(grade_name: str, session: Session = None):
         StudentGrade.Name == grade_name
     ).first()
     if matching_grade is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The student grade information for the student could not be created due to invalid parameters!")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The student grade record does not exist!")
     return matching_grade
 
 
-async def create_student_grade(student_grade: PydanticStudentGrade, session: Session = None):
+async def create_student_grade(student_grade: PydanticStudentGrade, session: Session = None) -> StudentGrade:
+    """
+    This method is used to create a new student grade level record from the provided grade name.
+
+    :param student_grade: The name of the student grade level that should be added to the database.
+    :type student_grade: PydanticStudentGrade, required
+    :param session: The database session used to create a new student grade record.
+    :type session: Session, optional
+    :return: The student grade level record
+    :rtype: StudentGrade
+    :raises HTTPException: If any of the parameters are invalid, or the student grade level already exists.
+    """
     student_grade = student_grade.student_grade
     if len(student_grade) == 0:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The student grade name cannot be empty!")
@@ -48,7 +85,19 @@ async def create_student_grade(student_grade: PydanticStudentGrade, session: Ses
     return new_student_grade
 
 
-async def remove_student_grade(student_grade: PydanticStudentGrade, session: Session = None):
+async def remove_student_grade(student_grade: PydanticStudentGrade, session: Session = None) -> StudentGrade:
+    """
+    This method is used to remove an existing student grade level from the database from the student grade name.
+    Please note that a student grade level that is currently used by a student cannot be deleted.
+
+    :param student_grade: The name of the student grade of the student grade level record that should be removed.
+    :type student_grade: PydanticStudentGrade, required
+    :param session: The database session used to delete the existing student grade level record.
+    :type session: Session, optional
+    :return: The student grade level record that was removed from the database.
+    :rtype: StudentGrade
+    :raises HTTPException: If any of the provided parameters are invalid, or the student grade level to be deleted does not exist or the grade level is currently in use.
+    """
     student_grade = student_grade.student_grade.lower().strip()
     if len(student_grade) == 0:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The student grade name cannot be empty!")
